100693b5e35d68e7b3a86e0dd87dd62ebc8e5e84
hippoom <hippoom@gmail.com>
2017-01-31 22:43:39 +0800

    initial commit

---
+++ src/test/groovy/com/restbucks/courtbooking/SpringBootApplicationContextTest.groovy
@@ -0,0 +1,17 @@
+package com.restbucks.courtbooking
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class SpringBootApplicationContextTest {
+
+    @Test
+    void itShouldLoadTheContext() throws Exception {
+
+    }
+}
