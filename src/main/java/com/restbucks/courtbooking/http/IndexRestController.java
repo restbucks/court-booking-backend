4a09df2fead2d0448480d15f8c8377be6b86ec39
hippoom <hippoom@gmail.com>
2017-01-31 23:30:55 +0800

    documenting rel index

--- src/main/java/com/restbucks/courtbooking/http/IndexRestController.java
+++ src/main/java/com/restbucks/courtbooking/http/IndexRestController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexRestController {
 
     /**
-     * list rels
+     * list rels.
      *
      * @Return links to rels
      */
