38bf64f5156680f1b22819f14b8814e49882925f
hippoom <hippoom@gmail.com>
2017-01-31 23:37:42 +0800

    Introducing courts rel

--- src/main/java/com/restbucks/courtbooking/http/CourtRestController.java
+++ src/main/java/com/restbucks/courtbooking/http/CourtRestController.java
@@ -1,7 +1,10 @@
 package com.restbucks.courtbooking.http;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
+import org.springframework.hateoas.Resource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +13,8 @@ public class CourtRestController {
 
     @RequestMapping(value = "/rel/courts", method = GET)
     public Object list() {
-        return null;
+        return new Resource<>("",
+            linkTo(methodOn(CourtRestController.class).list()).withSelfRel());
     }
 
 }
